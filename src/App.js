1442b39c71c85f4a1bceb329f0f4db39ed50bcb5
GUma321a <alexandrchernicov@gmail.com>
2022-10-15 00:17:01 +0300

    creacte react-router

--- src/App.js
+++ src/App.js
@@ -1,43 +1,24 @@
-import {useState, useEffect} from 'react';
+import { Routes, Route, } from 'react-router-dom';
+
 
 import './scss/app.scss';
 
 import Header from './Components/Header';
-import Sort from './Components/Sort';
-import Categories from './Components/Categories';
-import PizzaBlock from './Components/PizzaBlock';
-import SkeletonPizza from './Components/PizzaBlock/SkeletonPizza';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
+
 
 function App() {
-  let [items, setItems] = useState([])
-  let [loaded, setLoaded] = useState(true)
-
-  useEffect(() => {
-    fetch('https://63472e200484786c6e7c8b0f.mockapi.io/pizzas')
-      .then((res) => {
-        return res.json();
-      })
-      .then((items) => {
-        setItems(items);
-        setLoaded(false);
-      })
-  }, [])
-
-  
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Всі піцци</h2>
-          <div className="content__items">
-            {loaded ? [...new Array(6)].map((_, index) => <SkeletonPizza key={index} />) : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-          </div>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </div>
   );
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,5 @@
+function NotFound() {
+  return <h1>Нечего не найдено</h1>;
+}
+
+export default NotFound;
